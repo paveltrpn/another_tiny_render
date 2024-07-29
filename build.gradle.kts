15c856c68866114e3d0a6de3ce40ea7c1ceed50c
Pavel Turapin <paveltrpn@mail.ru>
2024-07-29 11:54:09 +0400

    add gradle.properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,3 +5,11 @@ allprojects {
     }
 }
 
+// print all tasks snippet
+//gradle.taskGraph.whenReady {
+//    val graph = getAllTasks()
+//    for (name in graph) {
+//        println(name)
+//    }
+//}
+
