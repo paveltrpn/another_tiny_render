c6f8e76ce30a67c4632e87fbf5ee82be9510b7aa
Pavel Turapin <paveltrpn@mail.ru>
2024-08-05 14:30:36 +0400

    import lwjgl

--- vm/build.gradle.kts
+++ vm/build.gradle.kts
@@ -1,16 +1,33 @@
-
 plugins {
     application
     kotlin("jvm") version "2.0.0"
 }
 
-//repositories {
-//    flatDir {
-//        dirs("lib")
-//    }
-//    flatDir {
-//        dirs("lib1", "lib2")
-//    }
+val lwjglVersion = "3.3.1"
+val lwjglNatives = "natives-linux"
+
+repositories {
+    mavenCentral()
+    // flatDir {
+    //      dirs("lib")
+    // }
+    // flatDir {
+    //      dirs("lib1", "lib2")
+    // }
+}
+
+dependencies {
+    // implementation("org.lwjgl:lwjgl:3.3.1")
+    implementation("org.lwjgl", "lwjgl", lwjglVersion)
+    implementation("org.lwjgl", "lwjgl-glfw", lwjglVersion)
+    implementation("org.lwjgl", "lwjgl-jemalloc", lwjglVersion)
+    implementation("org.lwjgl", "lwjgl-opengl", lwjglVersion)
+    implementation("org.lwjgl", "lwjgl-vulkan", lwjglVersion)
+    runtimeOnly("org.lwjgl", "lwjgl", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-glfw", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-jemalloc", classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-opengl", classifier = lwjglNatives)
+}
 
 application {
     mainClass = "kov.vm.VmKt"
--- vm/src/main/kotlin/vm.kt
+++ vm/src/main/kotlin/vm.kt
@@ -1,6 +1,15 @@
-
 package kov.vm
+
+import org.lwjgl.glfw.GLFW.*
 
 fun main(args: Array<String>) {
     println("hello virtual")
+
+    // Setup an error callback
+    // GLFWErrorCallback.createPrint(System.err).set()
+    if (!(glfwInit())) {
+        throw IllegalStateException("Unable to initialize window")
+    } else {
+        println("GLFW initialized")
+    }
 }
