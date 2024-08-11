66d27200e0e3ca76ef548534988d6a4d292f0da4
Pavel Turapin <paveltrpn@mail.ru>
2024-08-11 12:19:46 +0400

    add config test

--- modules/config/src/main/kotlin/config.kt
+++ modules/config/src/main/kotlin/config.kt
@@ -2,28 +2,94 @@ package modules.config
 
 import com.google.gson.Gson
 import java.io.File
+import java.io.FileNotFoundException
 
-
-class Config(file: String) {
+class Config(filePath: String) {
     data class Data(
+        var applicationName: String = "",
+        var fullscreen: Boolean = false,
+        var resizeable: Boolean = false,
         var windowWidth: Long = 0,
         var windowHeight: Long = 0,
+        var windowPosX: Long = 0,
+        var windowPosY: Long = 0,
+        var doublebuffer: Boolean = false,
+        var vsync: Boolean = false,
+        var debug: Boolean = false,
+        var engineName: String = "vk-eng",
+        var enableValidationLayers: Boolean = false,
+        var perspectiveCameraFov: Double = 50.0,
     )
 
     private var data = Data()
 
     init {
         // load json from file and serialize to data class
+        if (filePath.isEmpty()) {
+            throw IllegalArgumentException()
+        }
+
+        val file = File("src/test/resources/config.json")
+        if (!file.exists()) {
+            throw FileNotFoundException()
+        }
+
         val gson = Gson()
-        val jsonString = File(file).inputStream().readBytes().toString(Charsets.UTF_8)
+
+        val jsonString = file.inputStream().readBytes().toString(Charsets.UTF_8)
+
         data = gson.fromJson(jsonString, Data::class.java)
     }
 
+    fun applicationName(): String {
+        return data.applicationName
+    }
+
+    fun fullscreen(): Boolean {
+        return data.fullscreen
+    }
+
+    fun resizeable(): Boolean {
+        return data.resizeable
+    }
+
     fun windowWidth(): Long {
-        return 0
+        return data.windowWidth
     }
 
     fun windowHeight(): Long {
-        return 0
+        return data.windowHeight
+    }
+
+    fun windowPosX(): Long {
+        return data.windowPosX
+    }
+
+    fun windowPosY(): Long {
+        return data.windowPosY
+    }
+
+    fun doublebuffer(): Boolean {
+        return data.doublebuffer
+    }
+
+    fun vsync(): Boolean {
+        return data.vsync
+    }
+
+    fun debug(): Boolean {
+        return data.debug
+    }
+
+    fun engineName(): String {
+        return data.engineName
+    }
+
+    fun enableValidationlayers(): Boolean {
+        return data.enableValidationLayers
+    }
+
+    fun perspectiveCameraFov(): Double {
+        return data.perspectiveCameraFov
     }
 }
--- modules/config/src/test/kotlin/configTest.kt
+++ modules/config/src/test/kotlin/configTest.kt
@@ -1,10 +1,44 @@
 package modules.config
 
 import kotlin.test.Test
+import kotlin.test.assertFailsWith
+import java.io.File
+import kotlin.test.assertEquals
 
 class ConfigTest {
     @Test
-    fun dummy() {
-        println("config test dummy")
+    fun fileNameTest() {
+        assertFailsWith<IllegalArgumentException>(
+            message = "No exception found",
+            block = {
+                val emptyString = Config("")
+            }
+        )
     }
+
+    @Test
+    fun checkFileExist() {
+        val file = File("src/test/resources/config.json")
+        assert(file.exists())
+    }
+
+    @Test
+    fun checkValues() {
+        val config = Config("src/test/resources/config.json")
+
+        assertEquals("testApp", config.applicationName())
+        assertEquals(false, config.fullscreen())
+        assertEquals(false, config.resizeable())
+        assertEquals(800, config.windowWidth())
+        assertEquals(600, config.windowHeight())
+        assertEquals(100, config.windowPosX())
+        assertEquals(100, config.windowPosY())
+        assertEquals(true, config.doublebuffer())
+        assertEquals(true, config.vsync())
+        assertEquals(true, config.debug())
+        assertEquals("testEngine", config.engineName())
+        assertEquals(false, config.enableValidationlayers())
+        assertEquals(50.0, config.perspectiveCameraFov())
+    }
+
 }
